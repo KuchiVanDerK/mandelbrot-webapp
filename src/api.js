28e99f561b45f36d112ae58775a3dea3081c71c2
Philipp Kuchenmeister <kuchivanderk@gmail.com>
2018-12-19 16:52:03 +0100

    Update README

--- src/api.js
+++ src/api.js
@@ -3,10 +3,8 @@ import openSocket from 'socket.io-client';
 const socket = openSocket('http://localhost:3030');
 
 function subscribeToPixel(cb, width, height) {
-    console.log(cb);
-
     socket.on('pixel', pixel => cb(null, pixel));
-    socket.emit('subscribeToPixel', {maxX: width, maxY: height});
+    socket.emit('subscribeToPixel', {maxX: width/4, maxY: height/4});
 }
 
 export {subscribeToPixel};
